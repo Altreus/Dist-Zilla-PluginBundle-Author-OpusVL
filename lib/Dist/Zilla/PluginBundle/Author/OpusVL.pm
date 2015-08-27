7a90ad371a067ebee388d9659fb1686797c11142
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2015-08-27 14:14:38 +0100

    Remove auto stuff

--- lib/Dist/Zilla/PluginBundle/Author/OpusVL.pm
+++ lib/Dist/Zilla/PluginBundle/Author/OpusVL.pm
@@ -113,18 +113,3 @@ It is roughly equivalent to:
   [@Git]
   [Prereqs / TestMoreWithSubtests]
   Test::More = 0.96
-
-=head1 AUTHOR
-
-Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (c) 2015 OpusVL
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-See http://www.perl.com/perl/misc/Artistic.html
-
-=cut
